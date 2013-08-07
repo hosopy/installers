53aed55a5d8f4657142b307a128890508c43a0c1
hosopy <dev.hosopy@gmail.com>
2013-08-07 23:38:16 +0900

    add core location to FuelInstaller

--- src/Composer/Installers/FuelInstaller.php
+++ src/Composer/Installers/FuelInstaller.php
@@ -4,6 +4,7 @@ namespace Composer\Installers;
 class FuelInstaller extends BaseInstaller
 {
     protected $locations = array(
+        'core'    => 'fuel/{$name}/',
         'module'  => 'fuel/app/modules/{$name}/',
         'package' => 'fuel/packages/{$name}/',
     );
